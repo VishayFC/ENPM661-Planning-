2bd306c0f123cbae9789a5903c7f50a38fb36cf3
ADITYA JADHAV <amjadhav@umd.edu>
2021-02-22 13:19:42 -0500

    Root Node Creation

--- Project_1.py
+++ Project_1.py
@@ -31,32 +31,3 @@ q = Queue()
 
 root = np.array([[1,6,2,3],[9,5,7,4],[12,10,11,0],[13,14,15,8]])
 root = np.reshape(root,(4,4))
-
-vis = list()
-
-print('Root Node: ', root)
-
-q.enqueue(root)
-
-print('Queue after adding root : ',q.items)
-
-def currentnode():
-
-    cn1 = q.dequeue()
-    print('Queue after removing root : ',q.items)
-
-    vis.append(cn1)
-    
-    z = np.where( cn1 == 0 )
-    #print(a)
-    a = z[0][0]
-    b = z[1][0]
-    
-    return cn1,a,b
-
-
-k,i,j = currentnode()
-
-print('Visited List : ',vis)
-
-print('A B C : ',k,i,j)
